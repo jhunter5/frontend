07693c2912415b676781b906a78097d3eec49f81
jhunter5 <jhunter@unal.edu.co>
2025-02-04 21:00:18 -0500

    reparacion layout

--- app/inquilino-dashboard/layout.jsx
+++ app/inquilino-dashboard/layout.jsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { Settings, UserCheck, BarChart3, House, FolderClock, MapPinHouse, LogOut, Menu, CalendarDays} from 'lucide-react'
+import { Settings, UserCheck, BarChart3, House, FolderClock, MapPinHouse, LogOut, Menu, CalendarDays, FileText} from 'lucide-react'
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Separator } from "@/components/ui/separator"
