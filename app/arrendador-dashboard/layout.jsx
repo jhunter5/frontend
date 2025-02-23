6bfca49b65f0adb48db4519718c4c883578cd7a5
jhunter5 <jhunter@unal.edu.co>
2025-02-23 18:41:43 -0500

    redireccion logout corregida

--- app/arrendador-dashboard/layout.jsx
+++ app/arrendador-dashboard/layout.jsx
@@ -179,7 +179,7 @@ export default function DashboardLayout({ children }) {
             logoutParams: {
               returnTo: process.env.NODE_ENV === 'development'
               ? 'http://localhost:3000/'
-              : 'https://frontend-mocha-seven-79.vercel.app/',
+              : 'https://www.limitlessholdings.site/',
             }
           })
         }
